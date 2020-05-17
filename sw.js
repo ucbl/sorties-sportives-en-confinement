75a242507f9c7dc1a2d048bb7a064ec81de493cb
Lionel Médini <lionel.medini@univ-lyon1.fr>
2020-05-17 09:34:02 +0200

    Version PWA

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'v1';
+const CACHE_URLs = [
+  '/lionel.medini/cercle/',
+  '/lionel.medini/cercle/index.html',
+  '/lionel.medini/cercle/css/style.css',
+  '/lionel.medini/cercle/js/app.js',
+  'https://unpkg.com/leaflet@1.6.0/dist/leaflet.js',
+  'https://unpkg.com/leaflet@1.6.0/dist/leaflet.css',
+  'https://code.jquery.com/jquery-3.4.1.min.js'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches
+    .open(CACHE_NAME)
+    .then(function(cache) {
+      return cache.addAll(CACHE_URLs);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).catch(function() {
+      return fetch(event.request);
+    })
+  );
+});
